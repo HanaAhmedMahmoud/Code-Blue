f16542971733c74bfeac4059ea89b1369358ea86
anqilique <107088774+anqilique@users.noreply.github.com>
2025-04-23 17:03:07 +1200

    Angel - Animated Player Character

--- Scripts/States/PlayerIdle.gd
+++ Scripts/States/PlayerIdle.gd
@@ -8,9 +8,6 @@ func enter():
 	print("Player State -> Idle")
 	player = get_tree().get_first_node_in_group("Player")
 	anim_player = player.get_node("AnimationPlayer")
-	
-	# Replace this with idle animation.
-	anim_player.stop()
 
 func exit():
 	pass
--- Scripts/States/PlayerMove.gd
+++ Scripts/States/PlayerMove.gd
@@ -6,6 +6,8 @@ class_name PlayerMove
 var player
 var anim_player
 
+var player_direction
+
 func enter():
 	print("Player State -> Move")
 	player = get_tree().get_first_node_in_group("Player")
@@ -23,23 +25,35 @@ func physics_update(delta):
 	var anim_name = ""
 	var current_anim = anim_player.current_animation
 	
-	
 	if Input.is_action_pressed("right"):
 		player.velocity.x += 1
+		player_direction = "right"
+		
 	if Input.is_action_pressed("left"):
 		player.velocity.x -= 1
+		player_direction = "left"
+		
 	if Input.is_action_pressed("down"):
 		player.velocity.y += 1
-		anim_name = "move_down"
+		player_direction = "down"
+		
 	if Input.is_action_pressed("up"):
 		player.velocity.y -= 1
-	
-	if anim_name != "" and current_anim != anim_name:
-		anim_player.play(anim_name)
+		player_direction = "up"
 
 	if player.velocity.length() > 0:
 		player.velocity = player.velocity.normalized() * speed
+		
+		anim_name = "move_" + player_direction
+		
+		if anim_name != "" and current_anim != anim_name:
+			anim_player.play(anim_name)
+		
 	else:
+		anim_name = "idle_" + player_direction
+		anim_player.play(anim_name)
+		
+		anim_player.play(anim_name)  # Replace with correct idle anim.
 		get_parent().on_child_transition(self, "PlayerIdle")
 
 	player.position += player.velocity * delta
