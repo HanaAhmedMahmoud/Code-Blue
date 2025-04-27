04a4080cbf9762632df47c81f379ad4e925064ce
anqilique <107088774+anqilique@users.noreply.github.com>
2025-04-27 18:35:37 +1200

    Angel - Menu Theming

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -16,7 +16,7 @@ func _on_play_button_pressed() -> void:
 
 
 func _on_credits_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Credits.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Menu/Credits.tscn")
 
 func _on_controls_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Controls.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Menu/Controls.tscn")
